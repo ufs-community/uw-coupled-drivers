7f7e503e7420518ffc59670a1b5b3bdac741eb23
Paul Madden <136389411+maddenp-cu@users.noreply.github.com>
2025-11-20 09:30:15 -0700

    Updates for newer iotaa used by uwtools (#3)

--- coastal/coastal.py
+++ coastal/coastal.py
@@ -2,7 +2,7 @@
 A driver for the Coastal App coupled executable.
 """
 
-from iotaa import asset, task, tasks
+from iotaa import Asset, collection, task
 from uwtools.api.cdeps import CDEPS
 from uwtools.api.driver import DriverCycleBased
 from uwtools.api.fs import link
@@ -25,11 +25,11 @@ class Coastal(DriverCycleBased):
         links = self.config["links"]
         path = lambda fn: self.rundir / fn
         yield self.taskname("Linked files")
-        yield [asset(path(fn), path(fn).is_file) for fn in links.keys()]
+        yield [Asset(path(fn), path(fn).is_file) for fn in links.keys()]
         yield None
         link(config=links, target_dir=self.rundir)
 
-    @tasks
+    @collection
     def provisioned_rundir(self):
         """
         The run directory provisioned with all required content.
@@ -53,7 +53,7 @@ class Coastal(DriverCycleBased):
         """
         path = self.rundir / "RESTART"
         yield self.taskname("RESTART directory")
-        yield asset(path, path.is_dir)
+        yield Asset(path, path.is_dir)
         yield None
         path.mkdir(parents=True)
 
